3d968bbc9c7a95f073eb7044703dd277e40f20d7
LtnPjk <olle.olofsson97@gmail.com>
2018-09-24 17:23:21 +0200

    jag e boookkk

--- mqtt/mqttPub/mqttSub.py
+++ mqtt/mqttPub/mqttSub.py
@@ -6,35 +6,27 @@ import io
 import sys, getopt
 
 #init configurable vars
-pubTopic = ''
+subTopic = ''
 clientId = ''
 broker = ''
-
-message = '-'
-
 # help function on exception
 def help_me():
     print()
     print('List of valid arguments:')
     print('-b = broker adress')
-    print('-m = message')
     print('-p = topic')
 
 #parse any arguments passed on call
-pT = ''
-pM = ''
+sT = ''
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"p:m:b:", ["pT=","pM=","b="])
+    opts, args = getopt.getopt(sys.argv[1:],"s:b:", ["sT=","b="])
 except getopt.GetoptError:
     print('Error: invalid argument')
     help_me()
     sys.exit(2)
 for opt, arg in opts:
-    if opt in ('-m', '--pM'):
-        message = arg
-
-    elif opt in ('-p','--pT'):
-        pubTopic = arg
+    if opt in ('-s','--sT'):
+        subTopic = arg
 
     elif opt in('-b','--b'): 
         broker = arg
@@ -43,8 +35,8 @@ for opt, arg in opts:
 configFilepath = os.path.dirname(os.path.realpath(sys.argv[0])) + '/mqttConfig.ini'
 config = SafeConfigParser()
 config.read(configFilepath)
-if pubTopic == '':
-    pubTopic = config.get('default', 'pubTopic')
+if subTopic == '':
+    subTopic = config.get('default', 'subTopic')
 if clientId == '':
     clientId = config.get('default', 'clientId')
 if broker == '':
@@ -63,18 +55,20 @@ def on_connect(client, userdata, flags, rc):
 def on_disconnect(client, userdata, flags, rc=0):
     print("Disconnect result code "+str(rc))
 
+def on_message(client, userdata, msg):
+    time.sleep(1)
+    print(str(msg.payload.decode("utf-8")))
+
 print("Connect to: "+broker)
-print("Publish to: "+pubTopic)
+print("Subscribe to: "+subTopic)
 print("Client ID: "+clientId)
 print("")
 
-client = mqtt.Client(clientId) #new instance
+client = mqtt.Client('subTest') #new instance
 client.on_connect = on_connect #bind callback function
 client.on_disconnect = on_disconnect
-
+client.on_message = on_message
+print('KASODKASDK')
 client.connect(broker, 1883)
 client.loop_start()
-client.publish(pubTopic, message)
-time.sleep(1)
-client.loop_stop()
-client.disconnect()
+client.subscribe(subTopic)
